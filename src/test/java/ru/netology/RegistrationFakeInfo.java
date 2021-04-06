87779383df33bbe427ceeaf464ffb83f83db20cc
Eugen Mayer <Dyubomayer@gmail.com>
2021-04-06 22:14:07 +0500

    'FirstCommit'

---
+++ src/test/java/ru/netology/RegistrationFakeInfo.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class RegistrationFakeInfo {
+
+    private final String city;
+    private final String name;
+
+
+}
